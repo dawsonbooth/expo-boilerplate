7fcfbdde788bbd8738955fd33f730b3be99caa56
dawsonbooth <github@dawsonbooth.com>
2020-09-02 17:56:47 -0500

    Add custom theme

--- src/scenes/home/tab-two.component.tsx
+++ src/scenes/home/tab-two.component.tsx
@@ -3,7 +3,10 @@ import * as React from "react";
 import { StyleSheet } from "react-native";
 
 import { Text, TopNavigation, Divider, Layout } from "@ui-kitten/components";
-import { SafeAreaLayout, SaveAreaInset } from "../../components/safe-area-layout.component";
+import {
+  SafeAreaLayout,
+  SaveAreaInset,
+} from "../../components/safe-area-layout.component";
 
 export function TabTwoScreen() {
   const title = "Tab Two";
@@ -12,7 +15,7 @@ export function TabTwoScreen() {
     <SafeAreaLayout insets={SaveAreaInset.TOP} style={styles.screen}>
       <TopNavigation title={title} alignment="center" />
       <Divider />
-      <Layout style={styles.container}>
+      <Layout level="2" style={styles.container}>
         <Text category="h1">{title}</Text>
       </Layout>
     </SafeAreaLayout>
